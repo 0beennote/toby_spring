5ef9838c7d6236743b20f67095302941b1bd3fb6
Beennote <yb900721@naver.com>
2019-06-30 21:51:18 +0900

    Item.java

---
+++ contents/chapter3/yun/src/strategy/Item.java
@@ -0,0 +1,17 @@
+public class Item {
+	private final String name;
+	private final String price;
+	
+	public Item(String name, String price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public String getPrice() {
+		return price;
+	}
+	
+}
